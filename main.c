184eb171c2c89d54c9c50ccc9f2f050d155018d1
Caue <juliocaue22@gmail.com>
2026-04-17 23:50:10 -0300

    adicionadas funções e biblioteca para interagir com arquivo de perguntas (json). pegar_pergunta com erros.

--- main.c
+++ main.c
@@ -2,6 +2,31 @@
 #include <string.h>
 #include <windows.h>
 #include <stdlib.h>
+#include "bibliotecas_locais/cJSON.h"
+
+cJSON* ler_arquivo(){
+    //abrir arquivo
+    FILE *arquivo = fopen("perguntas.json","r");
+    if (arquivo == NULL){
+        printf("Erro: Não foi possivel abrir o arquivo\n");
+        return NULL;
+
+    //transformar o conteudo do arquivo em uma string
+    char buffer[1024];
+    int len = fread(buffer,1,sizeof(buffer),arquivo);
+
+    //parsear a data do arquivo json 
+    cJSON *json_parseado = cJSON_Parse(buffer);
+    if (json_parseado == NULL){
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL){
+            printf("Erro: %s\n", error_ptr);
+            }
+        }   
+    
+    return json_parseado;
+    }
+}
 
 //função responsavel pelo menu do programa
 int menu_programa(){
@@ -69,15 +94,33 @@ int menu_programa(){
     
 }
 
+char* pegar_pergunta(int pergunta_atual, cJSON* json_parseado){
+    char pergunta_str[10];
+    sprintf(pergunta_str,"%d",pergunta_atual);
+    cJSON *id_pergunta = cJSON_GetObjectItem(json_parseado,pergunta_str);
+    if (cJSON_IsString(id_pergunta) && (id_pergunta -> valuestring != NULL)){
+        return id_pergunta -> valuestring;
+    }
+}
+
 
 //função principal do programa
 void main(){
     //logica do programa fica aqui
     //usando funções menores
+    cJSON* json_parseado=ler_arquivo();
+    if (json_parseado == NULL){
+        printf("Erro: falha ao carregar dados\n");
+        getchar();
+    }
     //faz terminal usar utf-8
     SetConsoleOutputCP(65001);
     if (menu_programa()==1){
+        printf("testando");
         //chamada da função do jogo fica aqui
+        printf(pegar_pergunta(0, json_parseado));
+        getchar();
+        getchar();
     }
 
     //notas:
